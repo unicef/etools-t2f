323707f81b5ecf6bedda6897b4b0daef1512b72f
lordfuoco <lordfuoco@gmail.com>
2016-11-30 17:26:03 +0100

    linting

--- express.js
+++ express.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 var app = express();
-// var basedir = '/code/build/t2f/bundled/';
-var basedir = 'build/et2f/bundled/';
+var basedir = '/code/build/t2f/bundled/';
+// var basedir = 'build/et2f/bundled/';
 var basePort = 8080;
 var serverPort = ':8000';
 
-var backendProxy = proxy('/api/', {
-  target: 'http://127.0.0.1:8000/', //change with own IP
-  changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
-  logLevel: 'debug'
-});
+// var backendProxy = proxy('/api/', {
+//   target: 'http://127.0.0.1:8000/', //change with own IP
+//   changeOrigin: true,             // for vhosted sites, changes host header to match to target's host
+//   logLevel: 'debug'
+// });
 
 app.use('/et2f/',express.static(basedir));
 
@@ -19,7 +19,7 @@ app.use('/et2f/',express.static(basedir));
 // });
 
 
-app.use('/api/', backendProxy);
+// app.use('/api/', backendProxy);
 
 
 
