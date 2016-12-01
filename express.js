c7775ae9569289ec753ac71328e0e979e06ad0cb
lordfuoco <lordfuoco@gmail.com>
2016-12-01 11:04:32 +0100

    enabled refresh and direct url

--- express.js
+++ express.js
@@ -6,4 +6,14 @@ var basedir = '/code/build/t2f/bundled/';
 
 app.use('/t2f/', express.static(basedir));
 
+app.get(/.*service-worker\.js/, function(req, res) {
+  res.sendFile(basedir + 'service-worker.js');
+});
+
+app.use(function(req, res) {
+  res.sendFile(basedir + 'index.html');
+});
+
+
+
 app.listen(8080);
