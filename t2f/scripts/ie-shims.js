dd55cffee3bd2ffc3d1e4a7dd9beff9f6621006d
lordfuoco <lordfuoco@gmail.com>
2017-02-28 09:51:37 +0100

    final shims, some more css for ie

--- t2f/scripts/ie-shims.js
+++ t2f/scripts/ie-shims.js
@@ -1,114 +1,151 @@
 // jshint ignore: start
-if (!Array.prototype.find) {
-  Object.defineProperty(Array.prototype, 'find', {
-    value: function(predicate) {
-     // 1. Let O be ? ToObject(this value).
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
+(function() {
+  if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+      value: function(predicate) {
+       // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+
+        var o = Object(this);
+
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+
+        // 5. Let k be 0.
+        var k = 0;
+
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return kValue.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return kValue;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+
+        // 7. Return undefined.
+        return undefined;
       }
-
-      var o = Object(this);
-
-      // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
-
-      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function');
-      }
-
-      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-      var thisArg = arguments[1];
-
-      // 5. Let k be 0.
-      var k = 0;
-
-      // 6. Repeat, while k < len
-      while (k < len) {
-        // a. Let Pk be ! ToString(k).
-        // b. Let kValue be ? Get(O, Pk).
-        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-        // d. If testResult is true, return kValue.
-        var kValue = o[k];
-        if (predicate.call(thisArg, kValue, k, o)) {
-          return kValue;
-        }
-        // e. Increase k by 1.
-        k++;
-      }
-
-      // 7. Return undefined.
-      return undefined;
-    }
-  });
-}
-Number.isNaN = Number.isNaN || function(value) {
-    return typeof value === 'number' && isNaN(value);
-};
-
-if (!document.documentElement.classList) {
-    var prototype = Array.prototype;
-    var indexOf   = prototype.indexOf;
-    var slice     = prototype.slice;
-    var push      = prototype.push;
-    var splice    = prototype.splice;
-    var join      = prototype.join;
-
-    window.DOMTokenList = function(el) {
-        this._element = el;
-        if (el.className !== this._classCache) {
-            this._classCache = el.className;
-            if (!this._classCache) { return }
-            var classes = this._classCache.replace(/^\s+|\s+$/g, '').split(/\s+/);
-            console.log(classes);
-            var i;
-            for (i = 0; i < classes.length; i++) {
-                push.call(this, classes[i]);
-            }
-        }
-    };
-
-    window.DOMTokenList.prototype = {
-        add: function(token) {
-            if (this.contains(token)) { return; }
-            push.call(this, token);
-            this._element.className = slice.call(this, 0).join(' ');
-        },
-
-        contains: function(token) {
-            return indexOf.call(this, token) !== -1;
-        },
-
-        item: function(index) {
-            return this[index] || null;
-        },
-
-        remove: function(token) {
-            var i = indexOf.call(this, token);
-            if (i === -1) {
-                return;
-            }
-            splice.call(this, i, 1);
-            this._element.className = slice.call(this, 0).join(' ');
-        },
-
-        toString: function() {
-            return join.call(this, ' ');
-        },
-
-        toggle: function(token) {
-            if (!this.contains(token)) {
-                this.add(token);
-            } else {
-                this.remove(token);
-            }
-            return this.contains(token);
-        }
-    };
-
-    Object.defineProperty(Element.prototype, 'classList', {
-        get: function() {
-            return new window.DOMTokenList(this);
-        }
     });
-}
+  }
+  Number.isNaN = Number.isNaN || function(value) {
+      return typeof value === 'number' && isNaN(value);
+  };
+
+   // Used to resolve the internal `[[Class]]` of values
+  var toString = Object.prototype.toString;
+  // Used to resolve the decompiled source of functions
+  var fnToString = Function.prototype.toString;
+  // Used to detect host constructors (Safari > 4; really typed array specific)
+  var reHostCtor = /^\[object .+?Constructor\]$/;
+
+  // Compile a regexp using a common native method as a template.
+  // We chose `Object#toString` because there's a good chance it is not being mucked with.
+  var reNative = RegExp('^' +
+    // Coerce `Object#toString` to a string
+    String(toString)
+    // Escape any special regexp characters
+    .replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&')
+    // Replace mentions of `toString` with `.*?` to keep the template generic.
+    // Replace thing like `for ...` to support environments like Rhino which add extra info
+    // such as method arity.
+    .replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?') + '$'
+  );
+
+  function isNative(value) {
+    var type = typeof value;
+    return type == 'function'
+      // Use `Function#toString` to bypass the value's own `toString` method
+      // and avoid being faked out.
+      ? reNative.test(fnToString.call(value))
+      // Fallback to a host object check because some environments will represent
+      // things like typed arrays as DOM methods which may not conform to the
+      // normal native pattern.
+      : (value && type == 'object' && reHostCtor.test(toString.call(value))) || false;
+  }
+
+  //BEWARE: something in the toolchain import a faulty shim for classList, this fixes the problem
+  if (!isNative(window.DOMTokenList)) {
+      var prototype = Array.prototype;
+      var indexOf   = prototype.indexOf;
+      var slice     = prototype.slice;
+      var push      = prototype.push;
+      var splice    = prototype.splice;
+      var join      = prototype.join;
+
+      window.DOMTokenList = function(el) {
+          this._element = el;
+          var classes = el.getAttribute('class');
+          if (classes !== this._classCache) {
+              this._classCache = classes;
+              if (!this._classCache) { return; }
+              classes = this._classCache.replace(/^\s+|\s+$/g, '').split(/\s+/);
+              var i;
+              for (i = 0; i < classes.length; i++) {
+                  push.call(this, classes[i]);
+              }
+          }
+      };
+
+      window.DOMTokenList.prototype = {
+          add: function(token) {
+              if (this.contains(token)) { return; }
+              push.call(this, token);
+              var classes = slice.call(this, 0).join(' ');
+              this._element.setAttribute('class', classes);
+          },
+
+          contains: function(token) {
+              return indexOf.call(this, token) !== -1;
+          },
+
+          item: function(index) {
+              return this[index] || null;
+          },
+
+          remove: function(token) {
+              var i = indexOf.call(this, token);
+              if (i === -1) {
+                  return;
+              }
+              splice.call(this, i, 1);
+              var classes = slice.call(this, 0).join(' ');
+              this._element.setAttribute('class', classes);
+          },
+
+          toString: function() {
+              return join.call(this, ' ');
+          },
+
+          toggle: function(token) {
+              if (!this.contains(token)) {
+                  this.add(token);
+              } else {
+                  this.remove(token);
+              }
+              return this.contains(token);
+          }
+      };
+
+      Object.defineProperty(Element.prototype, 'classList', {
+          get: function() {
+              return new window.DOMTokenList(this);
+          }
+      });
+  }
+})();
